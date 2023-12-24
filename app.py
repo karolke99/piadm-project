1ffd5160e907eed6d2c42c141918265555fc9c46
karolke99 <karolke99@gmail.com>
2023-12-24 13:03:06 +0100

    integrate gui with viewer

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import tkinter
+
 import customtkinter
 
 from tkinter.colorchooser import askcolor
@@ -6,24 +8,30 @@ from tkinter.filedialog import askdirectory
 customtkinter.set_appearance_mode('system')
 customtkinter.set_default_color_theme('blue')
 
+from util import ModelViewer
+
+
+def convert_hex_to_rgb(hex_color):
+    return [int(hex_color[i:i + 2], 16) / 255.0 for i in (1, 3, 5)]
+
 
 class App(customtkinter.CTk):
 
     def __init__(self):
         super().__init__()
 
-        self.background_color = 'gray'
-        self.model_color = 'white'
+        self.background_color = '#999999'
+        self.model_color = '#FFFFFF'
         self.working_dir = '...'
 
         self.title('Marching cubes 3D models')
         self.geometry(f'{600}x{300}')
 
-        self.grid_columnconfigure((0, 1), weight=1)
+        self.grid_columnconfigure((0, 0), weight=1)
         self.grid_rowconfigure(0, weight=1)
 
         self.frame = customtkinter.CTkFrame(self)
-        self.frame.grid(row=0, column=0, sticky="nesw")
+        self.frame.grid(row=0, column=0)
 
         self.select_color_label = customtkinter.CTkLabel(master=self.frame, text='Select color for background')
         self.select_color_label.grid(row=0, column=1, padx=10, pady=5)
@@ -46,17 +54,35 @@ class App(customtkinter.CTk):
                                                                  command=self.select_working_dir)
         self.select_working_dir_button.grid(row=2, column=3, padx=10, pady=5)
 
+
+        self.generate_model_button = customtkinter.CTkButton(master=self.frame, border_width=2, text='Show 3D model',
+                                                             command=self.generate_model, state=tkinter.DISABLED)
+        self.generate_model_button.grid(row=3, column=2, padx=10, pady=5)
+
     def select_background_color(self):
-        self.background_color = askcolor(title='Select color')
-        self.select_color_button.configure(fg_color=f'{self.background_color[1]}')
+        _, self.background_color = askcolor(title='Select color')
+        if self.background_color is None:
+            self.background_color = '#999999'
+        self.select_color_button.configure(fg_color=f'{self.background_color}')
 
     def select_model_color(self):
-        self.model_color = askcolor(title='Select color')
-        self.select_model_color_button.configure(fg_color=f'{self.model_color[1]}')
+        _, self.model_color = askcolor(title='Select color')
+        if self.model_color is None:
+            self.model_color = '#FFFFFF'
+        self.select_model_color_button.configure(fg_color=f'{self.model_color}')
 
     def select_working_dir(self):
         self.working_dir = askdirectory(title='Select directory')
+        print(type(self.working_dir))
+        if self.working_dir not in [(), None, '', '...']:
+            self.generate_model_button.configure(state=tkinter.NORMAL)
+
         self.select_working_dir_button.configure(text=self.working_dir)
+
+    def generate_model(self):
+        rgb_background_color = convert_hex_to_rgb(self.background_color)
+        rgb_model_color = convert_hex_to_rgb(self.model_color)
+        mw = ModelViewer(rgb_background_color, rgb_model_color, self.working_dir)
 
 
 if __name__ == '__main__':
--- util.py
+++ util.py
@@ -1,70 +1,96 @@
 import numpy as np
 import vtk
-import imageio
 import glob
+from vtkmodules.util import numpy_support
 
 
-def dicom_to_vtk(volume, dicom_files):
+class ModelViewer():
+    def __init__(self, background_color, model_color, working_dir):
+        self.background_color = background_color
+        self.model_color = model_color
+        self.working_dir = working_dir
 
-    reader = vtk.vtkDICOMImageReader()
-    reader.SetDirectoryName(dicom_files)
-    reader.Update()
-    image_data = volume.DeepCopy(reader.GetOutput())
+        self.volume = vtk.vtkImageData()
+        self.colors = vtk.vtkNamedColors()
 
-    return image_data
+        self.vtk_image = self.dicom_to_vtk(self.volume)
 
+        self.surface = vtk.vtkMarchingCubes()
+        self.surface.SetInputData(self.volume)
+        self.surface.ComputeNormalsOn()
+        self.surface.SetValue(0, 1500)
 
-# def display_image(image_data):
+        self.renderer = vtk.vtkRenderer()
+        self.renderer.SetBackground(self.background_color)
+
+        self.render_window = vtk.vtkRenderWindow()
+        self.render_window.AddRenderer(self.renderer)
+        self.render_window.SetWindowName('Marching Cubes')
+
+        self.interactor = vtk.vtkRenderWindowInteractor()
+        self.interactor.SetRenderWindow(self.render_window)
+
+        self.mapper = vtk.vtkPolyDataMapper()
+        self.mapper.SetInputConnection((self.surface.GetOutputPort()))
+        self.mapper.ScalarVisibilityOff()
+
+        self.actor = vtk.vtkActor()
+        self.actor.SetMapper(self.mapper)
+        self.actor.GetProperty().SetColor(self.model_color)
+
+        self.renderer.AddActor(self.actor)
+
+        self.render_window.Render()
+        self.interactor.Start()
+
+    def dicom_to_vtk(self, volume):
+        reader = vtk.vtkDICOMImageReader()
+        reader.SetDirectoryName(self.working_dir)
+        reader.Update()
+        image_data = volume.DeepCopy(reader.GetOutput())
+        return image_data
+
+# def dicom_to_vtk(volume, dicom_files):
+#
+#     reader = vtk.vtkDICOMImageReader()
+#     reader.SetDirectoryName(dicom_files)
+#     reader.Update()
+#     image_data = volume.DeepCopy(reader.GetOutput())
+#
+#     return image_data
+
+# if __name__ == '__main__':
+#     dicom_path = 'dataset/ct/'
+#
+#     volume = vtk.vtkImageData()
+#     colors = vtk.vtkNamedColors()
+#
+#     vtk_image = dicom_to_vtk(volume, dicom_path)
+#
+#     surface = vtk.vtkMarchingCubes()
+#     surface.SetInputData(volume)
+#     surface.ComputeNormalsOn()
+#     surface.SetValue(0, 1500)
+#
+#     renderer = vtk.vtkRenderer()
+#     renderer.SetBackground(colors.GetColor3d('Black'))
+#
 #     render_window = vtk.vtkRenderWindow()
-#     render_window.SetWindowName('DICOM Viewer')
+#     render_window.AddRenderer(renderer)
+#     render_window.SetWindowName('Marching Cubes')
 #
-#     render_window_interactor = vtk.vtkRenderWindowInteractor()
-#     render_window_interactor.SetRenderWindow(render_window)
+#     interactor = vtk.vtkRenderWindowInteractor()
+#     interactor.SetRenderWindow(render_window)
 #
-#     image_viewer = vtk.vtkImageViewer()
-#     image_viewer.SetInputData(image_data)
-#     image_viewer.SetRenderWindow(render_window)
-#     image_viewer.SetupInteractor(render_window_interactor)
+#     mapper = vtk.vtkPolyDataMapper()
+#     mapper.SetInputConnection((surface.GetOutputPort()))
+#     mapper.ScalarVisibilityOff()
 #
-#     # image_viewer.SetSliceOrientationToXY()
+#     actor = vtk.vtkActor()
+#     actor.SetMapper(mapper)
+#     actor.GetProperty().SetColor(colors.GetColor3d('MistyRose'))
+#
+#     renderer.AddActor(actor)
 #
 #     render_window.Render()
-#     render_window_interactor.Start()
-
-
-if __name__ == '__main__':
-    dicom_path = 'dataset/ct/'
-
-    volume = vtk.vtkImageData()
-    colors = vtk.vtkNamedColors()
-
-    vtk_image = dicom_to_vtk(volume, dicom_path)
-
-    surface = vtk.vtkMarchingCubes()
-    surface.SetInputData(volume)
-    surface.ComputeNormalsOn()
-    surface.SetValue(0, 1500)
-
-    renderer = vtk.vtkRenderer()
-    renderer.SetBackground(colors.GetColor3d('Black'))
-
-    render_window = vtk.vtkRenderWindow()
-    render_window.AddRenderer(renderer)
-    render_window.SetWindowName('Marching Cubes')
-
-    interactor = vtk.vtkRenderWindowInteractor()
-    interactor.SetRenderWindow(render_window)
-
-    mapper = vtk.vtkPolyDataMapper()
-    mapper.SetInputConnection((surface.GetOutputPort()))
-    mapper.ScalarVisibilityOff()
-
-    actor = vtk.vtkActor()
-    actor.SetMapper(mapper)
-    actor.GetProperty().SetColor(colors.GetColor3d('MistyRose'))
-
-    renderer.AddActor(actor)
-
-    render_window.Render()
-    interactor.Start()
-
+#     interactor.Start()
